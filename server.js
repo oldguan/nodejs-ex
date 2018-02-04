8b024eb1b3f79f7da2a553db7e7c444492180374
seiya guan <oldguan@gmail.com>
2018-02-04 10:35:15 +0800

    ref js from github

--- server.js
+++ server.js
@@ -77,10 +77,6 @@ app.get('/', function (req, res) {
   }
 });
 
-app.get('/index.js', function (req, res) {
-  res.render('index.js', { pageCountMessage : null});
-});
-
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
